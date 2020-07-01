5d7bc88ebc4458643f84e8dfc7cedc1c2d82cdbc
Agnieszka Perkowska <agaper100@gmail.com>
2020-07-01 20:42:04 +0200

    created alerts

--- app.js
+++ app.js
@@ -48,6 +48,17 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        //Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -69,17 +80,28 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instatiate book
-    const book = new Book(title, author, isbn);
+    //Validate
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        //Instatiate book
+        const book = new Book(title, author, isbn);
 
-    //Add Book to UI
-    UI.addBookToList(book);
+        //Add Book to UI
+        UI.addBookToList(book);
 
-    //Clear Fields
-    UI.clearFields();
+        //Show success message
+        UI.showAlert('Book Added', 'success');
+
+        //Clear Fields
+        UI.clearFields();
+    }
 });
 
 //Event: remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-    UI.deleteBook(e.target)
+    UI.deleteBook(e.target);
+
+    //Book removed message
+    UI.showAlert('Book Removed', 'success');
 })
