1604c8838149736a72e8ffe74c4a7b9a22ded681
Agnieszka Perkowska <agaper100@gmail.com>
2020-07-01 20:17:28 +0200

    added book to UI

--- app.js
+++ app.js
@@ -41,6 +41,12 @@ class UI {
 
         list.appendChild(row);
     }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 }
 
 //Store Class: handle storage
@@ -49,5 +55,22 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: add a book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    //Prevent actual submit
+    e.preventDefault();
+    //Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //Instatiate book
+    const book = new Book(title, author, isbn);
+
+    //Add Book to UI
+    UI.addBookToList(book);
+
+    //Clear Fields
+    UI.clearFields();
+});
 
 //Event: remove a book
