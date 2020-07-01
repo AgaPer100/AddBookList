e858d5ab52d0a2cca32bc28f7bf0b8cbb51b1d96
Agnieszka Perkowska <agaper100@gmail.com>
2020-07-01 19:50:01 +0200

    created Book Class

---
+++ app.js
@@ -0,0 +1,19 @@
+//Book Class: represents a book
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+//UI Class: handle UI tasks
+
+//Store Class: handle storage
+
+//Event: display books
+
+//Event: add a book
+
+//Event: remove a book
